a5d177b48b9d7107b99aca5074b43b73b1214a93
Travis Desell <travis.desell@gmail.com>
2014-09-21 19:44:30 -0500

    Fixed css linking

--- header.php
+++ header.php
@@ -27,8 +27,8 @@ function print_header($page_title, $additional_scripts = "", $scheduler = "csg")
             -->
 
             <!-- Custom styles for this template -->
-            <link href='../csg/css/navbar-fixed-top.css' rel='stylesheet'>
-            <link href='../csg/css/custom.css' rel='stylesheet'>
+            <link href='./css/navbar-fixed-top.css' rel='stylesheet'>
+            <link href='./css/custom.css' rel='stylesheet'>
 
             <!-- jQuery (required by Bootstrap's JavaScript plugins) -->
             <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
