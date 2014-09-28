afc6791a72d9093e732a6f680f2d8d58349405d5
Travis Desell <travis.desell@gmail.com>
2014-09-28 16:12:32 -0500

    Added display badges script

---
+++ display_badges.php
@@ -0,0 +1,248 @@
+<?php
+
+$bossa_badge_info = array(
+    array( 'credit' => 48 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Emerald.png', 'name' => 'A quad emerald', 'value' => '48 weeks' ),
+    array( 'credit' => 45 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Jade.png', 'name' => 'A quad jade', 'value' => '45 weeks' ),
+    array( 'credit' => 42 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Saph.png', 'name' => 'A quad sapphire', 'value' => '42 weeks' ),
+    array( 'credit' => 39 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Turq.png', 'name' => 'A quad turquoise', 'value' => '39 weeks' ),
+    array( 'credit' => 36 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Amethyst.png', 'name' => 'A quad amethyst', 'value' => '36 weeks' ),
+    array( 'credit' => 33 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Gold.png', 'name' => 'A quad gold', 'value' => '33 weeks' ),
+    array( 'credit' => 30 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Silber.png', 'name' => 'A quad silver', 'value' => '30 weeks' ),
+    array( 'credit' => 27 * 7 * 24 * 60 * 60, 'img_src' => 'Feder4Bronze.png', 'name' => 'A quad bronze', 'value' => '27 weeks' ),
+
+    array( 'credit' => 24 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Emerald.png', 'name' => 'A triple emerald', 'value' => '24 weeks' ),
+    array( 'credit' => 22 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Jade.png', 'name' => 'A triple jade', 'value' => '22 weeks' ),
+    array( 'credit' => 20 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Saph.png', 'name' => 'A triple sapphire', 'value' => '20 weeks' ),
+    array( 'credit' => 18 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Turq.png', 'name' => 'A triple turquoise', 'value' => '18 weeks' ),
+    array( 'credit' => 16 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Amethyst.png', 'name' => 'A triple amethyst', 'value' => '16 weeks' ),
+    array( 'credit' => 14 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Gold.png', 'name' => 'A triple gold', 'value' => '14 weeks' ),
+    array( 'credit' => 12 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Silber.png', 'name' => 'A triple silver', 'value' => '12 weeks' ),
+    array( 'credit' => 10 * 7 * 24 * 60 * 60, 'img_src' => 'Feder3Bronze.png', 'name' => 'A triple bronze', 'value' => '10 weeks' ),
+
+    array( 'credit' =>  8 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Emerald.png', 'name' => 'A double emerald', 'value' => '8 weeks' ),
+    array( 'credit' =>  7 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Jade.png', 'name' => 'A double jade', 'value' => '7 weeks' ),
+    array( 'credit' =>  6 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Saph.png', 'name' => 'A double sapphire', 'value' => '6 weeks' ),
+    array( 'credit' =>  5 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Turq.png', 'name' => 'A double turquoise', 'value' => '5 weeks' ),
+    array( 'credit' =>  4 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Amethyst.png', 'name' => 'A double amethyst', 'value' => '4 weeks' ),
+    array( 'credit' =>  3 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Gold.png', 'name' => 'A double gold', 'value' => '3 weeks' ),
+    array( 'credit' =>  2 * 7 * 24 * 60 * 60, 'img_src' => 'Feder2Silber.png', 'name' => 'A double silver', 'value' => '2 weeks' ),
+    array( 'credit' =>      7 * 24 * 60 * 60, 'img_src' => 'Feder2Bronze.png', 'name' => 'A double bronze', 'value' => '1 week' ),
+
+    array( 'credit' =>      6 * 24 * 60 * 60, 'img_src' => 'FederEmerald.png', 'name' => 'An emerald', 'value' => '6 days' ),
+    array( 'credit' =>      5 * 24 * 60 * 60, 'img_src' => 'FederJade.png', 'name' => 'A jade', 'value' => '5 days' ),
+    array( 'credit' =>      4 * 24 * 60 * 60, 'img_src' => 'FederSaph.png', 'name' => 'A sapphire', 'value' => '4 days' ),
+    array( 'credit' =>      3 * 24 * 60 * 60, 'img_src' => 'FederTurq.png', 'name' => 'A turquoise', 'value' => '3 days' ),
+    array( 'credit' =>      2 * 24 * 60 * 60, 'img_src' => 'FederAmethyst.png', 'name' => 'An amethyst', 'value' => '2 days' ),
+    array( 'credit' =>          24 * 60 * 60, 'img_src' => 'FederGold.png', 'name' => 'A gold', 'value' => '1 day' ),
+    array( 'credit' =>          12 * 60 * 60, 'img_src' => 'FederSilber.png', 'name' => 'A silver', 'value' => '12 hours' ),
+    array( 'credit' =>           2 * 60 * 60, 'img_src' => 'FederBronze.png', 'name' => 'A bronze', 'value' => '2 hours' )
+);
+
+for ($i = 0; $i < count($bossa_badge_info); $i++) {
+    $bossa_badge_info[$i]['value'] .= " (" . number_format($bossa_badge_info[$i]['credit']) . " seconds)";
+}
+
+$wildlife_credit_badge_info = array(
+    array( 'credit' => 500000000, 'img_src' => 'credit_500000000_DoubleJadeEm.png', 'name' => 'A double jade-emerald', 'value' => '500 million' ),
+    array( 'credit' => 200000000, 'img_src' => 'credit_200000000_DoubleSaphTurq.png', 'name' => 'A double sapphire-turquoise', 'value' => '200 million' ),
+    array( 'credit' => 100000000, 'img_src' => 'credit_100000000_DoubleAmethRuby.png', 'name' => 'A double amethyst-ruby', 'value' => '100 million' ),
+    array( 'credit' =>  50000000, 'img_src' => 'credit_50000000_DoubleGold.png', 'name' => 'A double gold', 'value' => '50 million' ),
+    array( 'credit' =>  20000000, 'img_src' => 'credit_20000000_DoubleSilber.png', 'name' => 'A double silver', 'value' => '20 million' ),
+    array( 'credit' =>  10000000, 'img_src' => 'credit_10000000_DoubleBronze.png', 'name' => 'A double bronze', 'value' => '10 million' ),
+    array( 'credit' =>   5000000, 'img_src' => 'credit_5000000_JadeEm.png', 'name' => 'A jade-emerald', 'value' => '5 million' ),
+    array( 'credit' =>   2000000, 'img_src' => 'credit_2000000_Saphturq.png', 'name' => 'A sapphire-turquoise', 'value' => '2 million' ),
+    array( 'credit' =>   1000000, 'img_src' => 'credit_1000000_AmethRuby.png', 'name' => 'An amethyst-ruby', 'value' => '1 million' ),
+    array( 'credit' =>    500000, 'img_src' => 'credit_500000_Gold.png', 'name' => 'A gold', 'value' => '500 thousand' ),
+    array( 'credit' =>    100000, 'img_src' => 'credit_100000_Silber.png', 'name' => 'A silver', 'value' => '100 thousand' ),
+    array( 'credit' =>     10000, 'img_src' => 'credit_10000_Bronze.png', 'name' => 'A bronze ', 'value' => '10 thousand' )
+);
+
+$dna_credit_badge_info = array(
+    array( 'credit' =>  50000000, 'img_src' => 'dna_emerald.png', 'name' => 'An emerald', 'value' => '50 million' ),
+    array( 'credit' =>  10000000, 'img_src' => 'dna_ruby.png', 'name' => 'A ruby', 'value' => '10 million' ),
+    array( 'credit' =>   5000000, 'img_src' => 'dna_sapphire.png', 'name' => 'A sapphire', 'value' => '5 million' ),
+    array( 'credit' =>   1000000, 'img_src' => 'dna_gold.png', 'name' => 'A gold', 'value' => '1 million' ),
+    array( 'credit' =>    100000, 'img_src' => 'dna_silver.png', 'name' => 'A silver', 'value' => '100 thousand' ),
+    array( 'credit' =>     10000, 'img_src' => 'dna_bronze.png', 'name' => 'A bronze ', 'value' => '10 thousand' )
+);
+
+
+function get_bossa_badge_str($user) {
+    global $bossa_badge_info;
+
+    for ($i = 0; $i < count($bossa_badge_info); $i++) {
+        if ($user['bossa_total_credit'] > $bossa_badge_info[$i]['credit']) {
+            return $bossa_badge_info[$i]['img_src'];
+            break;
+        }
+    }
+
+    return "";
+}
+
+function get_dna_credit_badge_str($user) {
+    global $dna_credit_badge_info;
+
+    $result = query_boinc_db("SELECT sum(total) FROM credit_user WHERE userid = " . $user['id'] . " AND appid = 13");
+    $row = $result->fetch_assoc();
+
+    $dna_total_credit = $row['sum(total)'];
+
+    for ($i = 0; $i < count($dna_credit_badge_info); $i++) {
+        if ($dna_total_credit > $dna_credit_badge_info[$i]['credit']) {
+            return $dna_credit_badge_info[$i]['img_src'];
+            break;
+        }
+    }
+
+    return "";
+}
+
+function get_wildlife_credit_badge_str($user) {
+    global $wildlife_credit_badge_info;
+
+    $result = query_boinc_db("SELECT sum(total) FROM credit_user WHERE userid = " . $user['id'] . " AND (appid = 7 OR appid = 9 OR appid = 12)");
+    $row = $result->fetch_assoc();
+
+    $wildlife_total_credit = $row['sum(total)'];
+
+    for ($i = 0; $i < count($wildlife_credit_badge_info); $i++) {
+        if ($wildlife_total_credit > $wildlife_credit_badge_info[$i]['credit']) {
+            return $wildlife_credit_badge_info[$i]['img_src'];
+            break;
+        }
+    }
+
+    return "";
+}
+
+function get_dna_credit_badge($user) {
+    global $dna_credit_badge_info;
+    $badges = "";
+
+    $result = query_boinc_db("SELECT sum(total) FROM credit_user WHERE userid = " . $user->id . " AND appid = 13");
+    $row = $result->fetch_assoc();
+
+    $dna_total_credit = $row['sum(total)'];
+
+    for ($i = 0; $i < count($dna_credit_badge_info); $i++) {
+        if ($dna_total_credit > $dna_credit_badge_info[$i]['credit']) {
+            $badges .= "<img style='height:28px;' src='../dna/dna_badges/" . $dna_credit_badge_info[$i]['img_src'] . "' title='" . $dna_credit_badge_info[$i]['name'] . " badge for earning " . $dna_credit_badge_info[$i]['value'] . " DNA@Home credit.'></img>";
+            break;
+        }
+    }
+
+    return $badges;
+}
+
+
+function get_wildlife_credit_badge($user) {
+    global $wildlife_credit_badge_info;
+    $badges = "";
+
+    $result = query_boinc_db("SELECT sum(total) FROM credit_user WHERE userid = " . $user->id . " AND (appid = 7 OR appid = 9 OR appid = 12)");
+    $row = $result->fetch_assoc();
+
+    $wildlife_total_credit = $row['sum(total)'];
+
+    for ($i = 0; $i < count($wildlife_credit_badge_info); $i++) {
+        if ($wildlife_total_credit > $wildlife_credit_badge_info[$i]['credit']) {
+            $badges .= "<img style='height:28px;' src='../wildlife/wildlife_badges/" . $wildlife_credit_badge_info[$i]['img_src'] . "' title='" . $wildlife_credit_badge_info[$i]['name'] . " badge for earning " . $wildlife_credit_badge_info[$i]['value'] . " Wildlife@Home credit.'></img>";
+            break;
+        }
+    }
+
+    return $badges;
+}
+
+function get_bossa_badge($user) {
+    global $bossa_badge_info;
+    $badges = "";
+
+    for ($i = 0; $i < count($bossa_badge_info); $i++) {
+        if ($user->bossa_total_credit > $bossa_badge_info[$i]['credit']) {
+            $badges .= "<img style='height:28px;' src='../wildlife/wildlife_badges/" . $bossa_badge_info[$i]['img_src'] . "' title='" . $bossa_badge_info[$i]['name'] . " badge for watching " . $bossa_badge_info[$i]['value'] . " of validated video.'></img>";
+            break;
+        }
+    }
+
+    return $badges;
+}
+
+function get_badges($user) {
+    $badges = "";
+
+    if ($user->bossa_total_credit > 2 * 60 * 60) {
+        $badges .= "&nbsp;&nbsp;";
+        $badges .= get_bossa_badge($user);
+    }
+
+    $res = get_wildlife_credit_badge($user);
+    if ($res != "") $badges .= "&nbsp;&nbsp;" . $res;
+
+    $res = get_dna_credit_badge($user);
+    if ($res != "") $badges .= "&nbsp;&nbsp;" . $res;
+
+    return $badges;
+}
+
+function print_badge_table() {
+    global $bossa_badge_info, $wildlife_credit_badge_info, $dna_credit_badge_info;
+
+    echo "<div class='well' style='padding-top:10px; padding-bottom:10px;'>";
+
+    echo "<h3>Watched Video Badges <small>&nbsp;These badges are earned by <a href='../wildlife/video_selector.php'>watching video</a>.</small></h3>";
+    echo "<table class='table table-bordered table-striped'>";
+    echo "<thead>";
+    echo "<th>Badge</th> <th>Description</th>";
+    echo "</thead>";
+
+    echo "<tbody>";
+    for ($i = 0; $i < count($bossa_badge_info); $i++) {
+        echo "<tr>";
+        echo "<td style='text-align:center;'><img style='height:28px;' src='../wildlife/wildlife_badges/" . $bossa_badge_info[$i]['img_src'] . "' title='" . $bossa_badge_info[$i]['name'] . " badge for watching " . $bossa_badge_info[$i]['value'] . " of validated video.'></img></td>";
+        echo "<td style='vertical-align:middle;'>" . $bossa_badge_info[$i]['name'] . " badge for watching " . $bossa_badge_info[$i]['value'] . " of validated video.</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+
+    echo "<div class='well' style='padding-top:10px; padding-bottom:10px;'>";
+    echo "<h3>Wildlife@Home BOINC Credit Badges <small>&nbsp;These badges are earned by <a href='./instructions.php'>calculating workunits for Wildlife@Home with BOINC</a>.</small></h3>";
+    echo "<table class='table table-bordered table-striped'>";
+    echo "<thead>";
+    echo "<th>Badge</th> <th>Description</th>";
+    echo "</thead>";
+
+    echo "<tbody>";
+    for ($i = 0; $i < count($wildlife_credit_badge_info); $i++) {
+        echo "<tr>";
+        echo "<td style='text-align:center;'><img style='height:28px;' src='../wildlife/wildlife_badges/" . $wildlife_credit_badge_info[$i]['img_src'] . "' title='" . $wildlife_credit_badge_info[$i]['name'] . " badge for earning " . $wildlife_credit_badge_info[$i]['value'] . " Wildlife@Home credit.'></img></td>";
+        echo "<td style='vertical-align:middle;'>" . $wildlife_credit_badge_info[$i]['name'] . " badge for earning " . $wildlife_credit_badge_info[$i]['value'] . " Wildlife@Home credit.</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+
+    echo "<div class='well' style='padding-top:10px; padding-bottom:10px;'>";
+    echo "<h3>DNA@Home BOINC Credit Badges <small>&nbsp;These badges are earned by <a href='./instructions.php'>calculating workunits for DNA@Home with BOINC</a>.</small></h3>";
+    echo "<table class='table table-bordered table-striped'>";
+    echo "<thead>";
+    echo "<th>Badge</th> <th>Description</th>";
+    echo "</thead>";
+
+    echo "<tbody>";
+    for ($i = 0; $i < count($dna_credit_badge_info); $i++) {
+        echo "<tr>";
+        echo "<td style='text-align:center;'><img style='height:28px;' src='../dna/dna_badges/" . $dna_credit_badge_info[$i]['img_src'] . "' title='" . $dna_credit_badge_info[$i]['name'] . " badge for earning " . $dna_credit_badge_info[$i]['value'] . " DNA@Home credit.'></img></td>";
+        echo "<td style='vertical-align:middle;'>" . $dna_credit_badge_info[$i]['name'] . " badge for earning " . $dna_credit_badge_info[$i]['value'] . " DNA@Home credit.</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+}
+
+?>
