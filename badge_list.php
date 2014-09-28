fdc11b3531f387244dddbb3e812b8bc0bbea8b26
Travis Desell <travis.desell@gmail.com>
2014-09-28 16:04:09 -0500

    Fixed DNA badge issue pulling credit from wrong applications

---
+++ badge_list.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$cwd[__FILE__] = __FILE__;
+if (is_link($cwd[__FILE__])) $cwd[__FILE__] = readlink($cwd[__FILE__]);
+$cwd[__FILE__] = dirname($cwd[__FILE__]);
+
+require_once($cwd[__FILE__] . "/header.php");
+require_once($cwd[__FILE__] . "/navbar.php");
+require_once($cwd[__FILE__] . "/news.php");
+require_once($cwd[__FILE__] . "/footer.php");
+require_once($cwd[__FILE__] . "/my_query.php");
+require_once($cwd[__FILE__] . "/csg_uotd.php");
+require_once($cwd[__FILE__] . "/display_badges.php");
+
+print_header("Citizen Science Grid: Badges");
+print_navbar("Citizen Science Grid");
+
+echo "<div class='container'>";
+echo "<div class='row'>";
+echo "<div class='col-sm-12' style='margin-left:0px'>";
+echo "<div class='well' style='padding-top:10px; padding-bottom:10px;'>";
+
+echo "<p style='margin-top:5px; margin-bottom:5px;'>You can earn different badges for watching video or volunteering your computer to crunch workunits.  This page describes the different badges and how you can unlock them.</p>";
+echo "</div>";
+
+print_badge_table();
+
+echo "</div>";
+echo "</div>";
+echo "</div>";
+
+print_footer();
+
+echo "
+</body>
+</html>
+";
+
+?>
---
+++ daemons/export_badges.php
@@ -0,0 +1,55 @@
+#!/usr/bin/env php
+
+<?php
+
+
+$cwd[__FILE__] = __FILE__;
+if (is_link($cwd[__FILE__])) $cwd[__FILE__] = readlink($cwd[__FILE__]);
+$cwd[__FILE__] = dirname(dirname($cwd[__FILE__]));
+
+require_once($cwd[__FILE__] . "/my_query.php");
+require_once($cwd[__FILE__] . "/display_badges.php");
+
+echo "running export_badges.php at " . date('Y/m/d h:i:s a') . "\n";
+
+$result = query_boinc_db("SELECT id, name, email_addr, total_credit, bossa_total_credit, cross_project_id FROM user WHERE total_credit > 0 OR bossa_total_credit > 0", $boinc_db);
+
+$file = fopen("/projects/csg/download/badges.xml", "w");
+
+fwrite($file, "<users>\n");
+while ( ($row = $result->fetch_assoc()) != null) {
+    $user['id'] = $row['id'];
+    $user['bossa_total_credit'] = $row['bossa_total_credit'];
+    $user['cross_project_id'] = $row['cross_project_id'];
+    $user['email_addr'] = $row['email_addr'];
+
+    $cpid = md5($user['cross_project_id'] . $user['email_addr']);
+
+    fwrite($file, "<user>\n");
+    fwrite($file, "\t<id>" . $user['id'] . "</id>\n");
+    fwrite($file, "\t<cpid>" . $cpid . "</cpid>\n");
+    fwrite($file, "\t<bossa_credit>" . $user['bossa_total_credit'] . "</bossa_credit>\n");
+
+
+    $wildlife_credit_badge = get_wildlife_credit_badge_str($user);
+    $dna_credit_badge = get_dna_credit_badge_str($user);
+    $video_badge = get_bossa_badge_str($user);
+
+    if ($wildlife_credit_badge != "") {
+        fwrite($file, "\t<wildlife_credit_badge>" . $wildlife_credit_badge . "</wildlife_credit_badge>\n");
+    }
+
+    if ($dna_credit_badge != "") {
+        fwrite($file, "\t<dna_credit_badge>" . $dna_credit_badge . "</dna_credit_badge>\n");
+    }
+
+    if ($video_badge != "") {
+        fwrite($file, "\t<video_badge>" . $video_badge . "</video_badge>\n");
+    }
+
+    fwrite($file, "</user>\n");
+}
+
+fwrite($file, "</users>\n");
+
+?>
